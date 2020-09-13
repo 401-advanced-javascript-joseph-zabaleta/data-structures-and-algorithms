50403f2790ef7bf009118e3cb1b87b39aa58b45d
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-12 19:14:43 -0700

    Added working Code and Tests are passing

--- code-challenges/401/28-quickSort/quick-sort.test.js
+++ code-challenges/401/28-quickSort/quick-sort.test.js
@@ -7,7 +7,9 @@ describe('Testing Challenge 28 Quick Sort: ', () => {
 
     it('Should handle the sample array', () => {
         let arr = [8,4,23,42,16,15];
-        quickSort(arr);
+        let left = 0;
+        let right = arr.length - 1;
+        quickSort(arr, left, right);
         let expected = [4, 8, 15, 16, 23, 42];
 
         expect(arr).toEqual(expected);
@@ -16,7 +18,9 @@ describe('Testing Challenge 28 Quick Sort: ', () => {
 
     it('Should handle a reversed array', () => {
         let arr = [20, 18, 12, 8, 5, -2];
-        quickSort(arr);
+        let left = 0;
+        let right = arr.length - 1;
+        quickSort(arr, left, right);
         let expected = [-2, 5, 8, 12, 18, 20];
 
         expect(arr).toEqual(expected);
@@ -25,7 +29,9 @@ describe('Testing Challenge 28 Quick Sort: ', () => {
 
     it('Should handle a few unqiues', () => {
         let arr = [5,12,7,5,5,7];
-        quickSort(arr);
+        let left = 0;
+        let right = arr.length - 1;
+        quickSort(arr, left, right);
         let expected = [5, 5, 5, 7, 7, 12];
 
         expect(arr).toEqual(expected);
@@ -34,7 +40,9 @@ describe('Testing Challenge 28 Quick Sort: ', () => {
 
     it('Should handle nearly sorted array', () => {
         let arr = [2,3,5,7,13,11];
-        quickSort(arr);
+        let left = 0;
+        let right = arr.length - 1;
+        quickSort(arr, left, right);
         let expected = [2, 3, 5, 7, 11, 13];
 
         expect(arr).toEqual(expected);
