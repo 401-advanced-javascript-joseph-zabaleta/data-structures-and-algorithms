aa46f9f394c7a196929035047cf88c94f800cb83
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-09-23 09:30:42 -0700

    Added readme info and tests

---
+++ code-challenges/401/33-leftJoin/README.md
@@ -0,0 +1,26 @@
+# Left Join
+
+[Table of Contents](../../../README.md)
+
+[Link to the Code](./left-join.js)
+
+---
+
+## Challenge 33
+
+Implement a simplified LEFT JOIN for 2 Hashmaps.
+
+---
+
+## Examples
+
+![input](../../../assets/left_join/input.png)
+
+## Approach & Efficiency
+
+
+---
+
+## Solution
+
+![whiteboard}](../../../assets/left_join.png);
---
+++ code-challenges/401/33-leftJoin/left-join.test.js
@@ -0,0 +1,110 @@
+'use strict';
+
+const leftJoin = require('./left-join.js');
+
+describe('Testing Challenge 33: Left Join', () => {
+
+    it('Should handle example input #1', () => {
+
+        let hashmap1 = {
+            'fond': 'enamored',
+            'wrath': 'anger',
+            'diligent': 'employed',
+            'outift': 'garb',
+            'guide': 'usher',
+        }
+
+        let hashmap2 = {
+            'fond': 'averse',
+            'wrath': 'delight',
+            'diligent': 'idle',
+            'guide': 'follow',
+            'flow': 'jam',
+        }
+
+        let actual = leftJoin(hashmap1, hashmap2)
+        let expected = [
+            ['fond', 'enamored', 'averse'],
+            ['wrath', 'anger', 'delight'],
+            ['diligent', 'employed', 'idle'],
+            ['outift', 'garb', null],
+            ['guide', 'usher', 'follow']
+        ]
+
+        expect(actual).toEqual(expected);
+
+    });
+
+
+    it('Should handle example input #2', () => {
+
+        it('Should handle example input #1', () => {
+
+            let hashmap1 = {
+                'fond': 'averse',
+                'wrath': 'delight',
+                'diligent': 'idle',
+                'guide': 'follow',
+                'flow': 'jam',
+            }
+
+            let hashmap2 = {
+                'fond': 'enamored',
+                'wrath': 'anger',
+                'diligent': 'employed',
+                'outift': 'garb',
+                'guide': 'usher',
+            }
+
+
+
+            let actual = leftJoin(hashmap1, hashmap2)
+            let expected = [
+                ['fond', 'averse', 'enamored'],
+                ['wrath', 'delight', 'anger'],
+                ['diligent', 'idle', 'employed'],
+                ['guide', 'follow', 'usher'],
+                ['flow', 'jam', null]
+            ]
+
+            expect(actual).toEqual(expected);
+
+        });
+    });
+
+
+    it('Should handle a custom input', () => {
+
+        let hashmap1 = {
+            'fond': 'enamored',
+            'food': 'yummy',
+            'diligent': 'employed',
+            'guide': 'usher',
+            'flow': 'boom',
+            'computer': 'network',
+        }
+
+        let hashmap2 = {
+            'random': 'something',
+            'fond': 'averse',
+            'wrath': 'delight',
+            'diligent': 'idle',
+            'guide': 'follow',
+            'flow': 'jam',
+        }
+
+        let actual = leftJoin(hashmap1, hashmap2)
+        let expected = [
+            ['fond', 'enamored', 'averse'],
+            ['food', 'yummy', None],
+            ['diligent', 'employed', 'idle'],
+            ['guide', 'usher', 'follow'],
+            ['flow', 'boom', 'jam'],
+            ['computer', 'network', None]
+        ]
+
+        expect(actual).toEqual(expected);
+
+    });
+
+});
